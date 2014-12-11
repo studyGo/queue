fc3028303332415d3266ebed5716d09f46f39d26
kaleesi <85316319@qq.com>
2014-12-11 12:55:25 +0800

    add file

---
+++ queue.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+    "fmt"
+    "container/list"
+)
+
+type Queue struct {
+    v interface{}
+    list *list.List
+}
+
+func Create() *Queue {
+    return &Queue{
+        v : "",
+        list : list.New(),
+    }
+}
+
+func (q Queue) RPush(v string) {
+    q.list.PushBack(v)
+}
+
+func (q Queue) LPush(v string) {
+    q.list.PushFront(v)
+}
+
+func (q Queue) LPop() interface{} {
+    ele := q.list.Front()
+    q.v = q.list.Remove(ele)
+    return q.v
+}
+
+func (q Queue) RPop() interface{} {
+    ele := q.list.Back()
+    q.v = q.list.Remove(ele)
+    return q.v
+}
+
+func (q Queue) RemoveAll() {
+    q.list = list.New()
+}
+
+func (q Queue) Len() int {
+    return q.list.Len()
+}
+
+func (q Queue) LastVal() interface{} {
+    return q.v
+}
+
+func main () {
+    q := Create()
+    fmt.Println(q.v)
+    q.RPush("demo")
+    q.RPush("s")
+    fmt.Println(q.LPop())
+    fmt.Println(q.LPop())
+}
